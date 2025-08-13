c6ae4a4da65abac0380d64f37c29eb137c62a04b
Anju <rajkrishnananju@gmail.com>
2025-08-14 01:33:46 +0400

    migrate down index update

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,5 +1,5 @@
-DROP INDEX IF EXISTS idx_trips_vehicle_start_time;
-DROP INDEX IF EXISTS idx_vehicle_id;
+DROP INDEX IF EXISTS idx_trips_start_time;
+DROP INDEX IF EXISTS idx_trips_vehicle_id;
 
 DROP TABLE IF EXISTS trips;
 DROP TABLE IF EXISTS vehicle;
